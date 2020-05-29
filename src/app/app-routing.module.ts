6f1bd9ef93ea6198d8db86bf4f8b1160408a43f5
Vetox <karthikeyan1997@gmail.com>
2020-05-30 04:32:32 +0530

    =initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterChartComponent } from './scatter-chart/scatter-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "line", component: LineChartComponent
+},
+{
+  path: "pie", component: PieComponent
+}, {
+  path: "scatter", component: ScatterChartComponent
+},
+{
+  path: "radar", component: RadarChartComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterChartComponent } from './scatter-chart/scatter-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartComponent,
+    PieComponent,
+    ScatterChartComponent,
+    RadarChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
